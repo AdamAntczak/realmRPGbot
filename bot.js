d7a5467f1487ad60640daf84c11433548f466fe8
AdamAntczak <43954014+AdamAntczak@users.noreply.github.com>
2019-11-12 20:57:12 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,8 +11,14 @@ client.on('message', message => {
   if (message.content.substring(0, 1) == '>') {
     let args = message.content.substring(1).split(' ');
     let cmd = args[0];
+    let subcmd = args[1];
     if (cmd == "status") {
       message.reply('I\'m online! Hello!');
+    }
+    else if (cmd == "roll") {
+      if (subcmd.split('')[0] == 'd') {
+        message.reply('not finished yet lmao')
+      }
     }
   };
 });
