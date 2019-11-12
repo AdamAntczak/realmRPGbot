a36105818ecb6ef291770f66316fac5017ac7e3b
AdamAntczak <43954014+AdamAntczak@users.noreply.github.com>
2019-11-12 21:44:00 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,9 @@ client.on('message', message => {
   if (message.content.substring(0, 1) == '>') {
     let args = message.content.substring(1).split(' ');
     let cmd = args[0];
-    let subcmd = args[1];
+    if (args.length > 1) {
+      let subcmd = args[1];
+    }
     if (cmd == "status") {
       message.reply('I\'m online! Hello!');
     }
@@ -22,10 +24,13 @@ client.on('message', message => {
     else if (cmd == "roll") {
       let check = /^\d+$/;
       if (subcmd.split('')[0] == 'd') {
-        let sides = subcmd.splice(1)
+        let sides = subcmd.split('d')[1]
         if ( check.test(sides) ) {
           if (sides > 1) {
             message.reply( String( Math.ceil(Math.random()*sides) ) )
+          }
+          else {
+            message.reply('The number of sides needs to be above 1!')
           }
         }
         else {
@@ -38,6 +43,9 @@ client.on('message', message => {
           if (sides > 1) {
             message.reply( String( Math.ceil(Math.random()*sides) ) )
           }
+          else {
+            message.reply('The number of sides needs to be above 1!')
+          }
         }
         else {
           message.reply('You need to give a correct value for the number of sides! (Must be above 1 and be a number)')
@@ -47,9 +55,9 @@ client.on('message', message => {
         message.reply('You need to use the correct format for this! (e.g. >roll 2d20 OR >roll d12)')
       }
       
-    }
-  };
-});
+    };//closes if statement checking if cmd is 'roll'
+  };//closes if statement checking for '>'
+});//close client.on
 
  
 client.login(process.env.BOT_TOKEN);
