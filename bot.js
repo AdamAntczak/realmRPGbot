171bb2fe069785c0e835f1ce40301a83af00eda6
AdamAntczak <43954014+AdamAntczak@users.noreply.github.com>
2019-11-18 18:24:49 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,8 +11,9 @@ client.on('message', message => {
   if (message.content.substring(0, 1) == '>') {
     let args = message.content.substring(1).split(' ');
     let cmd = args[0];
+    let subcmd;
     if (args.length > 1) {
-      let subcmd = args[1];
+      subcmd = args[1];
     }
     if (cmd == "status") {
       message.reply('I\'m online! Hello!');
